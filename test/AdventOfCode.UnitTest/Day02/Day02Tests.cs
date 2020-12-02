d5d36e6048d251ffd12179f8c9acb6300cba1bc2
deloorc <cedric.deloor@delaware.pro>
2020-12-02 09:22:51 +0100

    Add base unit test

---
+++ test/AdventOfCode.UnitTest/Day02/Day02Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.UnitTest
+{
+    [TestClass]
+    public class Day01Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc" }, 2)]
+        public void Sample(string[] input, int expectedValid)
+        {
+            // Arrange
+            //var repair = new ReportRepair(sum);
+
+            // Act
+            var output = 2;
+
+            // Assert
+            Assert.AreEqual(expectedValid, output);
+        }
+    }
+}
