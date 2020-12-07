c9aadbc6c424ebf65e4457f5f7c85d708a24a949
Cédric De Loor ☕ <deloorc@users.noreply.github.com>
2020-12-07 12:47:18 +0100

    Solve day 06 (#6)

---
+++ test/AdventOfCode.UnitTest/Day06/Day06Tests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode.Core.Day06;
+
+namespace AdventOfCode.UnitTest.Day06
+{
+    [TestClass]
+    public class Day06Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] {
+            "abc",
+            "",
+            "a",
+            "b",
+            "c",
+            "",
+            "ab",
+            "ac",
+            "",
+            "a",
+            "a",
+            "a",
+            "a",
+            "",
+            "b"
+        }, 11)]
+        public void ExamplePartOne(string[] input, int expected)
+        {
+            // Arrange
+            var customs = new TobogganCustoms(input);
+
+            // Act
+            var sum = customs.AnalyseAnswers();
+
+            // Assert
+            Assert.AreEqual(expected, sum);
+        }
+
+        [DataTestMethod]
+        [DataRow(new[] {
+            "abc",
+            "",
+            "a",
+            "b",
+            "c",
+            "",
+            "ab",
+            "ac",
+            "",
+            "a",
+            "a",
+            "a",
+            "a",
+            "",
+            "b"
+        }, 6)]
+        public void ExamplePartTwo(string[] input, int expected)
+        {
+            // Arrange
+            var customs = new TobogganCustoms(input);
+
+            // Act
+            var sum = customs.AnalyseGroupAnswers();
+
+            // Assert
+            Assert.AreEqual(expected, sum);
+        }
+    }
+}
