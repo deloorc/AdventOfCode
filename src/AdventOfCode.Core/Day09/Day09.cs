c2f5ab040b41e5c8905e9a020df9998424daee98
deloorc <cedric.deloor@delaware.pro>
2020-12-08 18:33:33 +0100

    Setup day09

---
+++ src/AdventOfCode.Core/Day09/Day09.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Core.Day09
+{
+    public class Day09Puzzle
+    {
+        private readonly IEnumerable<string> _input;
+
+        public Day09Puzzle(IEnumerable<string> input) => _input = input;
+    }
+}
---
+++ test/AdventOfCode.UnitTest/Day09/Day09Tests.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode.Core.Day09;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.UnitTest.Day09
+{
+    [TestClass]
+    public class Day09Tests
+    {
+        [DataTestMethod]
+        [DataRow(new[] {
+           ""
+        }, 5)]
+        public void ExamplePartOne(string[] input, int expected)
+        {
+            // Arrange
+            var x = new Day09Puzzle(input);
+            // Act
+
+            // Assert
+            //Assert.AreEqual(expected, x);
+        }
+    }
+}
